4acb2e628ea61777cabe91e41a96ed202a75d79b
Mandzhonson <manzhos.mihail@mail.ru>
2026-01-28 20:41:25 +0300

    update models.go

--- internal/models/models.go
+++ internal/models/models.go
@@ -3,7 +3,8 @@ package models
 import "time"
 
 type Events struct {
-	UserID int       `json:"user_id"`
-	Date   time.Time `json:"date"`
-	Event  string    `json:"event"`
+	EventID int       `json:"event_id"`
+	UserID  int       `json:"user_id"`
+	Date    time.Time `json:"date"`
+	Event   string    `json:"event"`
 }
